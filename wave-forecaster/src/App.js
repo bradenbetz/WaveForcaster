fc13ba2b7197eefd788a6106c24ae1defe57563e
Braden Betz <bradenb@hawaii.edu>
2021-07-12 20:46:35 -1000

    Removed Web Scraping
    Will use NOAA API to fetch JSON data instead of scraping it.

--- wave-forecaster/src/App.js
+++ wave-forecaster/src/App.js
@@ -1,36 +1,13 @@
 import './App.css';
 import React from "react";
-import rp from "request-promise";
-import * as cheerio from "react-dom/test-utils";
-
-const url = "https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/station_page.php?station=51201"
 
 class App extends React.Component {
-  state = {};
 
-    componentDidMount() {
-        rp(url)
-            .then(html => {
-                let $ = cheerio.load(html);
-                let data = [];
-
-                $("#data table span").each(
-                    function(i, element) {
-                    let stuffs = $(this)
-                        .prepend()
-                        .text()
-                    stuffs.push(data);
-                });
-            })
-            .catch(function(err) {
-                console.log("crawl failed");
-            });
-    }
 
   render() {
     return (
         <div>
-            {this.state.data}
+            {this.data}
             <p>test</p>
         </div>
     )
