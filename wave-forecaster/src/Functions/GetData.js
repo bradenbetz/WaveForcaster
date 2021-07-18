611dbe997ab12cf0f01bcd5d5a67cabbc0c0a7e0
Braden Betz <bradenb@hawaii.edu>
2021-07-18 09:29:01 -1000

    added moesif plugin
    Added moesif plugin to my chrome to get around the CORS error, will create my own node.js server later.

--- wave-forecaster/src/Functions/GetData.js
+++ wave-forecaster/src/Functions/GetData.js
@@ -5,20 +5,24 @@ Grab .txt file from https://www.ndbc.noaa.gov/data/realtime2/51201.txt
  */
 export default class GetData {
     static #url = "https://www.ndbc.noaa.gov/data/realtime2/51201.txt";
+    static file;
 
     static retrieve = (swell, period) => {
-    //retrieve .txt file with buoy data, use ajax instead of .get to send headers
+        //retrieve .txt file with buoy data, use ajax instead of .get to send headers
         $.ajax({
             type: 'GET',
             url: this.#url,
-            dataType: 'jsonp',
+            dataType: 'text',
             success: [ function (data) {
-                console.log(data);
+                this.file = data;
+                console.log(this.file);
+
             }],
             error: function () {
                 console.log("error");
             },
         });
+    //retrieve .txt file with buoy data, use ajax instead of .get to send headers
 // parse txt file to get the most up to date and store into swell and period respectively
 // once data has been manipulated. display through props
     }
