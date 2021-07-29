7764907060e790b350fe01cc0ad2e93dbd5f7ce6
Braden Betz <bradenb@hawaii.edu>
2021-07-28 18:02:30 -1000

    moved retrieve to app.js
    moved it there to make it easier to change the state and send the props to the calculations component, which sends to the spot check component, which sends back to app.js for rendering

--- wave-forecaster/src/startup/App.js
+++ wave-forecaster/src/startup/App.js
@@ -2,14 +2,24 @@ import '../style/App.css';
 import React from 'react';
 import NavBar from "../ui/NavBar.js";
 import SpotCheck from "../ui/SpotCheck";
-import {retrieveData} from "../Functions/Functions";
+import $ from "jquery";
+
+/*TODO
+- Create function to retrieve .txt file - done
+- display the calculated wave height working on it/ finished grabbing necessary swell height, period and direction
+- make it fancy
+-Then create a favorites feature that allows users to save spots based on preference and group them together through account
+
+ */
 
 const url = "https://www.ndbc.noaa.gov/data/realtime2/51201.txt";
 
 class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = { apiResponse: "" };
+        this.state = {appData: {}};
     }
 
     callAPI() {
@@ -17,18 +27,29 @@ class App extends React.Component {
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }));
     }
-/*TODO
-- Create function to retrieve .txt file - done
-- display the calculated wave height working on it
-- make it fancy
--Then create a favorites feature that allows users to save spots based on preference and group them together through account
 
- */
+    retrieveData (url) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'text',
+            success: [ function (data) {
+                parseFile(data);
+            }],
+            error: function () {
+                alert("BUOY IS DOWN");
+            },
+        });
+        function parseFile (data) {
+            const myArr = data.split('  ');
+            console.log(myArr[30], myArr[31], myArr[32]);
+        }
+    }
 
-componentDidMount() {
-    retrieveData(url);
-    this.callAPI();
-}
+    componentDidMount() {
+    this.retrieveData(url);
+    //this.callAPI();
+    }
 
     render() {
     return (
